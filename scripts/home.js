2dbab0ec6fb3816a0a4b243d7fe3d5b2099a1185
Colin <colinkinzel@gmail.com>
2018-10-16 11:25:15 -0500

    fixing slide for posts

--- scripts/home.js
+++ scripts/home.js
@@ -1,3 +1,9 @@
 function on_page_load() {
-    $(".post").toggle("slide");
+    $(".post").hide()
+    $(".post")
+    .css("margin-left",-$(this).width())
+    .animate({
+        marginLeft:0
+    }, 700);
+    $(".post").show()
 } 
