eec3d9ba11d7ac9457ba9f565dd2420c2509c7fd
Colin <colinkinzel@gmail.com>
2019-01-16 12:32:06 -0600

    Update home.js

--- scripts/home.js
+++ scripts/home.js
@@ -1,8 +1,4 @@
 function onPageLoad() {
-    $(".post").hide().show();
-    $(".post")
-    .css("margin-left",-$(this).width())
-    .animate({
-        marginLeft:0
-    }, 700);
+    $(".post").show();
+    $(".post").toggle("slide");
 } 
