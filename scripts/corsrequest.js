8ec4a602c15f04b0f9bfe330fc4c326102bc1c77
Colin <colinkinzel@gmail.com>
2019-02-02 20:53:22 -0600

    handles connection for PlayerCount.html

---
+++ scripts/corsrequest.js
@@ -0,0 +1,34 @@
+// Create the XHR object.
+function createCORSRequest(method, url) {
+  var xhr = new XMLHttpRequest();
+  if ("withCredentials" in xhr) {
+    // XHR for Chrome/Firefox/Opera/Safari.
+    xhr.open(method, url, true);
+  } else if (typeof XDomainRequest != "undefined") {
+    // XDomainRequest for IE.
+    xhr = new XDomainRequest();
+    xhr.open(method, url);
+  } else {
+    // CORS not supported.
+    xhr = null;
+  }
+  return xhr;
+}
+
+
+// Make the actual CORS request.
+function makeCorsRequest(url, callback) {
+
+  var xhr = createCORSRequest('GET', url);
+  // Response handlers.
+  xhr.onload = function() {
+    var text = xhr.responseText;
+    callback(JSON.parse(text).players);
+  };
+
+  xhr.onerror = function() {
+  };
+
+  xhr.send();
+  return;
+}
