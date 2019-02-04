e0c88b264235dbc57481f46aa13e5fec3e851f5f
Colin <colinkinzel@gmail.com>
2019-02-04 07:16:35 -0600

    Update playercount.js

--- scripts/playercount.js
+++ scripts/playercount.js
@@ -23,7 +23,8 @@ function buildChart() {
             }]
         },
         options: {
-            responsive: false,
+            responsive: true,
+            maintainAspectRatio: false,
             scales: {
                 yAxes: [{
                     ticks: {
