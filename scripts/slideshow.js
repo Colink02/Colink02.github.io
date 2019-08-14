b48207bd730dc8f72a5b80bb4859ba88f808278e
Colin <colinkinzel@gmail.com>
2019-08-14 12:33:59 -0500

    Update slideshow.js

--- scripts/slideshow.js
+++ scripts/slideshow.js
@@ -21,6 +21,12 @@ function showSlides(n) {
   for (i = 0; i < dots.length; i++) {
       dots[i].className = dots[i].className.replace(" active", "");
   }
-  slides[slideIndex-1].style.display = "block";  
-  dots[slideIndex-1].className += " active";
+  if(slides != undefined) {
+    console.log(slides);
+    slides[slideIndex-1].style.display = "block";
+  }
+  if(dots != undefined) {
+    console.log(dots);
+    dots[slideIndex-1].className += " active";
+  }
 }
