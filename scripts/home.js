a289f69d2577ba8dd29daca75bab25d4e3c9f37c
Colin <colinkinzel@gmail.com>
2019-08-19 11:39:39 -0500

    Update home.js

--- scripts/home.js
+++ scripts/home.js
@@ -6,3 +6,15 @@ $(function onPageLoad() {
           $(this).find('.dropdown-menu').first().stop(true, true).slideUp();
      });
 });
+//Credit: https://www.taniarascia.com/smooth-scroll-to-id-with-jquery/
+$('a[href*="#"]').on('click', function(e) {
+  e.preventDefault()
+
+  $('html, body').animate(
+    {
+      scrollTop: $($(this).attr('href')).offset().top,
+    },
+    500,
+    'linear'
+  )
+})
