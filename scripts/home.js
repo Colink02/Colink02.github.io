6657075955d4a85834fc85eb3b017461a9d2d4bd
Colink02 <colinkinzel@gmail.com>
2019-09-25 00:29:54 -0500

    Animate those cards :D

--- scripts/home.js
+++ scripts/home.js
@@ -1,17 +1,16 @@
 $(function onPageLoad() {
-     $('.dropdown').on('show.bs.dropdown', function () {
-          $(this).find('.dropdown-menu').first().stop(true, true).slideDown();
-     });
-     $('.dropdown').on('hide.bs.dropdown', function () {
-          $(this).find('.dropdown-menu').first().stop(true, true).slideUp();
-     });
+  $('.dropdown').on('show.bs.dropdown', function () {
+    $(this).find('.dropdown-menu').first().stop(true, true).slideDown(100);
+  });
+  $('.dropdown').on('hide.bs.dropdown', function () {
+    $(this).find('.dropdown-menu').first().stop(true, true).slideUp(100);
+  });
 });
 //Credit: https://www.taniarascia.com/smooth-scroll-to-id-with-jquery/
-$('a[href*="#"]').on('click', function(e) {
+$('a[href*="#"]').on('click', function (e) {
   e.preventDefault()
 
-  $('html, body').animate(
-    {
+  $('html, body').animate({
       scrollTop: $($(this).attr('href')).offset().top,
     },
     500,
